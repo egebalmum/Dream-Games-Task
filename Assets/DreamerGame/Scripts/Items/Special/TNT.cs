94960a69f95508d7b4466b201501aba4469776b6
Ege Balmum <ebalmum19@ku.edu.tr>
2024-02-06 23:16:15 +0300

    Remove unused namespaces

--- Assets/DreamerGame/Scripts/Items/Special/TNT.cs
+++ Assets/DreamerGame/Scripts/Items/Special/TNT.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor.Animations;
 using UnityEngine;
 
 public class TNT : Item
--- Assets/DreamerGame/Scripts/Managers/LevelManager/LevelManager.cs
+++ Assets/DreamerGame/Scripts/Managers/LevelManager/LevelManager.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Events;
 
